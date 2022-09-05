5c44d89e49832603ffe96c4fb129be2cb779b505
omar atbd <omar.atbd@gmail.com>
2022-09-05 20:59:06 +0100

    test of authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function login(AuthRequest $request)
+    {
+        return response()->json(['test' => []]);
+    }
+}
